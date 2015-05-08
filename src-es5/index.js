1cd8b99bbdcd04a08a81cdc262fae8c951b3d47d
Kuzya <alexkuz@gmail.com>
2015-05-08 16:37:53 +0300

    build

--- src-es5/index.js
+++ src-es5/index.js
@@ -6,13 +6,17 @@ Object.defineProperty(exports, '__esModule', {
 
 var _interopRequireDefault = function (obj) { return obj && obj.__esModule ? obj : { 'default': obj }; };
 
+var _objectWithoutProperties = function (obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; };
+
 var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } };
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 var _inherits = function (subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; };
 
-var _markdown = require('markdown');
+var _markdown = require('markdown-it');
+
+var _markdown2 = _interopRequireDefault(_markdown);
 
 var _React$PropTypes$Component = require('react');
 
@@ -26,19 +30,97 @@ var _assign = require('lodash/object/assign');
 
 var _assign2 = _interopRequireDefault(_assign);
 
+var _reduce = require('lodash/collection/reduce');
+
+var _reduce2 = _interopRequireDefault(_reduce);
+
+var _zipObject = require('lodash/array/zipObject');
+
+var _zipObject2 = _interopRequireDefault(_zipObject);
+
+var _sortBy = require('lodash/collection/sortBy');
+
+var _sortBy2 = _interopRequireDefault(_sortBy);
+
 'use strict';
 
 var DEFAULT_TAGS = {
   html: 'span'
 };
 
+function convertTree(tokens, nested) {
+  var branch = [];
+
+  if (!nested) {
+    branch.push('html');
+  }
+
+  function getBlock(tkn, tkns) {
+    var block = [];
+    block.push(tkn.tag);
+    if (tkn.attrs) {
+      var attrs = _zipObject2['default'](_sortBy2['default'](tkn.attrs, 0));
+
+      if (attrs.alt === '') {
+        attrs.alt = tkn.children[0].content;
+      }
+
+      block.push(attrs);
+    }
+
+    if (tkn.content && !tkn.children) {
+      block.push(tkn.content);
+      return block;
+    }
+
+    return block.concat(convertTree(tkns, true));
+  }
+
+  var token = tokens.shift();
+  while (token && token.nesting !== -1) {
+    if (token.nesting === 1) {
+      if (token.hidden) {
+        branch = branch.concat(convertTree(tokens, true));
+      } else {
+        branch.push(getBlock(token, tokens));
+      }
+    }
+
+    if (token.nesting === 0) {
+      if (token.type === 'inline') {
+        branch = branch.concat(convertTree(token.children, true));
+      } else if (token.type === 'text') {
+        branch.push(token.content);
+      } else if (token.type === 'softbreak') {
+        branch.push('\n');
+      } else {
+        branch.push(getBlock(token, tokens));
+      }
+    }
+    token = tokens.shift();
+  }
+  return branch;
+}
+
 function mdReactFactory() {
   var options = arguments[0] === undefined ? {} : arguments[0];
   var onIterate = options.onIterate;
   var _options$tags = options.tags;
   var tags = _options$tags === undefined ? DEFAULT_TAGS : _options$tags;
-  var dialect = options.dialect;
+  var presetName = options.presetName;
   var markdownOptions = options.markdownOptions;
+  var _options$enableRules = options.enableRules;
+  var enableRules = _options$enableRules === undefined ? [] : _options$enableRules;
+  var _options$disableRules = options.disableRules;
+  var disableRules = _options$disableRules === undefined ? [] : _options$disableRules;
+  var _options$plugins = options.plugins;
+  var plugins = _options$plugins === undefined ? [] : _options$plugins;
+
+  var md = _markdown2['default'](presetName, markdownOptions).enable(enableRules).disable(disableRules);
+
+  md = _reduce2['default'](plugins, function (m, plugin) {
+    return m.use(plugin);
+  }, md);
 
   function iterateTree(tree) {
     var level = arguments[1] === undefined ? 0 : arguments[1];
@@ -59,8 +141,7 @@ function mdReactFactory() {
   }
 
   return function (text) {
-    var tree = _markdown.markdown.toHTMLTree(text, dialect, markdownOptions);
-
+    var tree = convertTree(md.parse(text));
     return iterateTree(tree);
   };
 }
@@ -80,13 +161,11 @@ var MDReactComponent = (function (_Component) {
     key: 'render',
     value: function render() {
       var _props = this.props;
-      var onIterate = _props.onIterate;
-      var tags = _props.tags;
       var text = _props.text;
-      var dialect = _props.dialect;
-      var markdownOptions = _props.markdownOptions;
 
-      return mdReactFactory({ onIterate: onIterate, tags: tags, dialect: dialect, markdownOptions: markdownOptions })(text);
+      var props = _objectWithoutProperties(_props, ['text']);
+
+      return mdReactFactory(props)(text);
     }
   }], [{
     key: 'propTypes',
@@ -94,8 +173,11 @@ var MDReactComponent = (function (_Component) {
       text: _React$PropTypes$Component.PropTypes.string.isRequired,
       onIterate: _React$PropTypes$Component.PropTypes.func,
       tags: _React$PropTypes$Component.PropTypes.object,
-      dialect: _React$PropTypes$Component.PropTypes.string,
-      markdownOptions: _React$PropTypes$Component.PropTypes.object
+      presetName: _React$PropTypes$Component.PropTypes.string,
+      markdownOptions: _React$PropTypes$Component.PropTypes.object,
+      enableRules: _React$PropTypes$Component.PropTypes.array,
+      disableRules: _React$PropTypes$Component.PropTypes.array,
+      plugins: _React$PropTypes$Component.PropTypes.array
     },
     enumerable: true
   }]);
